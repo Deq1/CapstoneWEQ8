fcce2a84e19fafe780b5ad439b35fa6e3ceb76c7
Deq1 <wdq18620919873@163.com>
2024-01-13 11:50:59 -0500

    commit

--- src/ui/WEQ8Analyser.ts
+++ src/ui/WEQ8Analyser.ts
@@ -64,7 +64,7 @@ export class WEQ8Analyser {
     }
     path.lineTo(w, h);
 
-    ctx.fillStyle = "rgba(30, 30, 60, 0.7)";
+    ctx.fillStyle = "rgba(20, 100, 60, 0.7)";
     ctx.fill(path);
 
     ctx.strokeStyle = "rgb(155, 155, 255)";
---
+++ src/ui/WEQ8InputAnalyser.ts
@@ -0,0 +1,2 @@
+import { WEQ8Runtime } from "../runtime";
+import { WEQ8Analyser } from "./WEQ8Analyser";
